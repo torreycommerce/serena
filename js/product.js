d4636cb4af4bb5a767e805499b6a5175b6282a64
Simon Jaspar <simonj@torreycommerce.com>
2015-08-12 12:22:42 -0700

    Integrated variant options management in product page

--- js/product.js
+++ js/product.js
@@ -44,7 +44,7 @@ function VariantsManager (variants, variant_options, isCollection) {
                     }
                 }
             });
-
+            
             var filteredVariants = self.getFilteredVariants(selectedValues2);
             var generatedSelectsData = self.generateSelectsData(filteredVariants);
 
@@ -192,24 +192,24 @@ function VariantsManager (variants, variant_options, isCollection) {
         $.each(variant_options, function(index, option){
             var selectName = option.name;
             var optionArray = option.values;
-            
+
             //Color styling
             if( selectName.toLowerCase() == "color"){
                 if(self.isCollection){
-                    var div = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName, name: selectName, class: "color-details-collection"}); 
+                    var div = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName, name: selectName, class: ""}); 
                 }else{
-                    var div = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName, name: selectName, class: "color-details"});
+                    var div = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName, name: selectName, class: ""});
                 }
      
-                var ul = $('<ul>', {class: "swatches Color"});  
-                var span = $('<span>', {class: "selected-color"}).append(
+                var ul = $('<ul>', {class: ""});  
+                var button = $('<button>', {class: "", onclick: "dropdown_trigger(this)"}).append(
                                 $('<strong>', {}).text(selectName.slice(0,1).toUpperCase()+selectName.slice(1,selectName.length) + ":  ") 
-                            );
+                            ); 
 
             }else{//size (default) styling
-                var div = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName, name: selectName, class: "size-details"});           
-                var ul = $('<ul>', {class: "swatches-size Size"});  
-                var span = $('<span>', {class: "selected-size"}).append(
+                var div = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName, name: selectName, class: ""});           
+                var ul = $('<ul>', {class: ""});  
+                var button = $('<button>', {class: "", onclick: "dropdown_trigger(this)"}).append(
                                 $('<strong>', {}).text(selectName.slice(0,1).toUpperCase()+selectName.slice(1,selectName.length) + ":  ") 
                             );
             }
@@ -222,27 +222,29 @@ function VariantsManager (variants, variant_options, isCollection) {
                                 "data-toggle": "tooltip",
                                 "title": self.outOfStock
                     })
-                    .append(
-                            self.getATag(selectName, optionValue)
-                    ).click(function(){
+                    // .append(
+                    //         self.getATag(selectName, optionValue)
+                    // )
+                    .text(optionValue.toUpperCase())
+                    .click(function(){
                         self.updateVariants(selectName, optionValue);
                     })  
                 );
             });
 
-            var span_selected = $('<span>', {class: "", id: "selected-"+selectName+"-"+self.product_id}).text("");
+            var content = $('<div>', {id: "variation-selector-"+self.product_id+"-"+selectName+"-content", name: selectName, class: ""});
+            content.append(ul);
 
-            div.append(span);
-            div.append(span_selected);
-            div.append(ul);
+            div.append(button);
+            div.append(content);
 
             if(self.isCollection){
                 $(self.selector).append(div);
             }else{
-                var row = $('<div>', {class: "row no-margin"});
-                row.append(div);
-                var div = $('<div>', {class: "col-md-6 no-padding"});
-                div.append(row);
+                // var row = $('<div>', {class: "row no-margin"});
+                // row.append(div);
+                // var div = $('<div>', {class: "col-md-6 no-padding"});
+                // div.append(row);
                 $(self.selector).append(div);
             }
         });
@@ -301,4 +303,13 @@ function VariantsManager (variants, variant_options, isCollection) {
         //Bluilding HTML Select elements
         self.buildChips(self.variant_options);
     }
+}
+
+function dropdown_trigger(element){
+    var selector = "#"+$(element).parent().attr("id")+"-content";
+    var elem = $(selector);
+    if(elem.is(":visible"))
+        elem.hide();
+    else
+        elem.show();
 }
